be1202ef01e58be9153e04c8a7cfa8d94ebf0619
Abroradumalikov <abrorabdumalikov1072@gmail.com>
2024-10-02 17:51:29 +0500

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,121 +1,136 @@
-const form = document.querySelector(".form");
-const userName = document.querySelector("#name");
-const userError = document.querySelector(".user-error");
-const userPassword = document.querySelector("#password");
-const passwordError = document.querySelector(".password-error");
 const box = document.querySelector(".box");
-form.addEventListener("submit", (event) => {
-  event.preventDefault();
-  validateUserName();
-  validateUserEmail();
-  validatepasswordError();
-});
-userName.addEventListener("input", () => {
-  validateUserName();
-});
-function validateUserName() {
-  const userNameValue = userName.value.trim();
 
-  if (
-    userNameValue.length < 3 ||
-    userNameValue.length > 25 ||
-    !/[A-Z]/.test(userNameValue)
-  ) {
-    userError.classList.add("active");
-    userName.classList.remove("success-input");
-    userName.classList.add("error-input");
-  } else {
-    userError.classList.remove("active");
-    userName.classList.add("error-input");
-    userName.classList.add("success-input");
-  }
+const state = fetch("https://restcountries.com/v3.1/all");
+state
+  .then((state) => {
+    if (state.status === 200) {
+      return state.json();
+    }
+  })
+  .then((datas) => {
+    const country = (countries) => {
+      box.innerHTML = "";
+      countries.forEach((data) => {
+        const div = document.createElement("div");
+        const info = document.createElement("div");
+        const flags = document.createElement("img");
+        const name = document.createElement("p");
+        const population = document.createElement("p");
+        const region = document.createElement("p");
+        const capital = document.createElement("p");
+        const area = document.createElement("p");
+
+        flags.src = data.flags.svg;
+        flags.alt = `${data.name.common} flag`;
+        name.textContent = `${data.name.common}`;
+        population.textContent = `Population: ${data.population
+          .toLocaleString("en-US", { useGrouping: true })
+          .replace(/,/g, " ")}`;
+        region.textContent = `Region: ${data.region}`;
+        capital.textContent = `Capital: ${data.capital}`;
+        area.textContent = `Area: ${data.area
+          .toLocaleString("en-US", { useGrouping: true })
+          .replace(/,/g, " ")}`;
+
+        div.classList.add("div");
+        flags.classList.add("img");
+        name.classList.add("name");
+        region.classList.add("region");
+        capital.classList.add("capital");
+        area.classList.add("area");
+        info.classList.add("info");
+
+        info.appendChild(flags);
+        info.appendChild(name);
+        info.appendChild(population);
+        info.appendChild(region);
+        info.appendChild(capital);
+        info.appendChild(area);
+        div.appendChild(info);
+        box.appendChild(div);
+      });
+    };
+
+    country(datas);
+
+    const europe = document.querySelector(".europe");
+    europe.addEventListener("click", () => {
+      const regionFilterBtn = datas.filter(
+        (country) => country.region === "Europe"
+      );
+      country(regionFilterBtn);
+    });
+
+    const africa = document.querySelector(".africa");
+    africa.addEventListener("click", () => {
+      const regionFilterBtn = datas.filter(
+        (country) => country.region === "Africa"
+      );
+      country(regionFilterBtn);
+    });
+
+    const asia = document.querySelector(".asia");
+    asia.addEventListener("click", () => {
+      const regionFilterBtn = datas.filter(
+        (country) => country.region === "Asia"
+      );
+      country(regionFilterBtn);
+    });
+
+    const americas = document.querySelector(".americas");
+    americas.addEventListener("click", () => {
+      const regionFilterBtn = datas.filter(
+        (country) => country.region === "Americas"
+      );
+      country(regionFilterBtn);
+    });
+
+    const menu = document.querySelector(".menu");
+    const filterMenu = document.querySelector(".filter__menu");
+    const regionMenu = document.querySelector(".filter-region-menu");
+
+    menu.addEventListener("click", () => {
+      filterMenu.classList.toggle("menu1");
+    });
+
+    const filterBtn = document.querySelector(".filter-btn");
+    filterBtn.addEventListener("click", () => {
+      const filteredCountry = datas.sort((a, b) => b.population - a.population);
+      country(filteredCountry);
+    });
+
+    const area = document.querySelector(".area-btn");
+    area.addEventListener("click", () => {
+      const areaSorted = datas.sort((a, b) => b.area - a.area);
+      country(areaSorted);
+    });
+  })
+  .catch((error) => console.log(error));
+
+const btn = document.querySelector(".btn");
+const body = document.querySelector("body");
+const mode = document.getElementById("dark-btn");
+const light = document.getElementById("lght-btn");
+
+const modeLocal = localStorage.getItem("mode");
+
+if (modeLocal) {
+  body.classList.add("bg-color");
+  mode.classList.toggle("hidden");
+  light.classList.toggle("hidden");
 }
-const userEmail = document.querySelector("#email");
-const emailError = document.querySelector(".email-error");
-userEmail.addEventListener("input", () => {
-  validateUserEmail();
+
+const toggleModeBtn = () => {
+  mode.classList.toggle("hidden");
+  light.classList.toggle("hidden");
+  body.classList.toggle("bg-color");
+};
+
+mode.addEventListener("click", () => {
+  toggleModeBtn();
+  localStorage.setItem("mode", "bg-color");
 });
-function validateUserEmail() {
-  const emailVelue = userEmail.value.trim();
-  const chekUser = userRegex(emailVelue);
-
-  if (!userRegex(emailVelue)) {
-    emailError.classList.add("active");
-    userEmail.classList.remove("success-input");
-    userEmail.classList.add("error-input");
-  } else {
-    emailError.classList.remove("active");
-    userEmail.classList.add("error-input");
-    userEmail.classList.add("success-input");
-  }
-}
-
-function userRegex(email) {
-  let emailRegex = /^[a-zA-Z0-9._%+-]+@gmail\.com$/;
-  return emailRegex.test(email);
-}
-function validatepasswordError() {
-  const passwordVelue = userPassword.value.trim();
-  if (passwordVelue.length < 8 || !/[A-Z]/.test(passwordVelue)) {
-    passwordError.classList.add("active");
-    box.classList.add("box-error");
-    box.classList.remove("box-success");
-  } else {
-    passwordError.classList.remove("active");
-    box.classList.remove("box-error");
-    box.classList.add("box-success");
-  }
-}
-
-box.addEventListener("input", () => {
-  validatepasswordError();
+light.addEventListener("click", () => {
+  toggleModeBtn();
+  localStorage.setItem("mode", "");
 });
-const img = document.querySelector("img");
-const inp = document.querySelector(".input-img");
-let n = 0;
-img.addEventListener("click", () => {
-  n++;
-  if (n % 2 != 0) {
-    inp.type = "text";
-    img.src = "./yes.svg";
-  } else {
-    inp.type = "password";
-    img.src = "./no.svg";
-  }
-});
-document
-  .querySelector("#confirm-password")
-  .addEventListener("input", validatePasswordError);
-
-function validatePasswordError() {
-  const password = document.querySelector("#password");
-  const confirm = document.querySelector("#confirm-password");
-  const passwordError = document.querySelector(".password-error");
-  const confirmError = document.querySelector(".confirm-error");
-
-  // Regular expression for password validation
-  const passwordPattern =
-    /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&*!]).{8,}$/;
-
-  // Validate password strength
-  if (!passwordPattern.test(password.value)) {
-    passwordError.classList.remove("active");
-    password.classList.add("error-input");
-    password.classList.remove("success-input");
-  } else {
-    passwordError.classList.add("active");
-    password.classList.remove("error-input");
-    password.classList.add("success-input");
-  }
-
-  // Validate confirm password
-  if (password.value !== confirm.value) {
-    confirmError.classList.add("active");
-    confirm.classList.add("error-input");
-    confirm.classList.remove("success-input");
-  } else {
-    confirmError.classList.remove("active");
-    confirm.classList.remove("error-input");
-    confirm.classList.add("success-input");
-  }
-}
